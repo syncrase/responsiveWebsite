924d476aa8970ed8007a946b9611abc794ba2aea
syncrase <pierre.taquet@gmail.com>
2015-11-12 10:33:35 +0000

    Add the w3schools tutoriel about the bootstrap company theme

---
+++ W3SchoolsBootstrapTuto/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '0-head.html'?>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<?php include '1-navbar.html'?>
+	<?php include '2-CONT-header.html'?>
+	<?php include '3-CONT-about.html'?>
+	<?php include '4-CONT-values.html'?>
+	<?php include '5-CONT-services.html'?>
+	<?php include '6-CONT-portfolio.html'?>
+	<?php include '7-CONT-caroussel.html'?>
+	<?php include '8-CONT-pricing.html'?>
+	<?php include '9-CONT-contacts.html'?>
+	<div id="googleMap" style="height:400px;width:90%;"></div>
+	<?php include '10-CONT-footer.html'?>
+</body>
+</html>
