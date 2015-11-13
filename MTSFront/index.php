e2266b5b9d0d25759f82c8c7adc8ddb3b9c97faf
syncrase <pierre.taquet@gmail.com>
2015-11-13 11:25:07 +0000

    add the front finished front end of the more than school project

---
+++ MTSFront/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include './inclusions/HEAD.html'?>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<?php include './inclusions/index/navbar.html'?>
+	<?php include './inclusions/index/indexHeader.html'?>
+	<?php include './inclusions/index/about.html'?>
+	<?php include './inclusions/index/coursParticuliers.html'?>
+	<?php include './inclusions/index/professeurs.html'?>
+	
+	<?php include './inclusions/index/footer.html'?>
+</body>
+</html>
